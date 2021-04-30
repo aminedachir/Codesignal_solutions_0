019d9597127a4f9e087d2edc89c9db6c8a8cebb7
aminedachir <dachiramine110@gmail.com>
2021-04-30 02:14:24 +0100

    add input year

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-year = 1000
+year = input("enter year:")
 def centuryFromYear(year):
     a = 1
     b = 100
     c = b/100
     for i in range(20):
-        if(a<=year<=b):
+        if(a<=int(year)<=b):
             print(int(c))
         a = a + 100
         b = b + 100
