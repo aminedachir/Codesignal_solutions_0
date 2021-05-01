60c0c505fd81ce9308f5f4891e36e5cb7f0b6637
aminedachir <dachiramine110@gmail.com>
2021-05-02 00:38:02 +0100

    modifications

--- main4.py
+++ main4.py
@@ -1,6 +1,6 @@
 inputArray = input('enter array :')
 def adjacentElementsProduct(inputArray):
-    return max(i nputArray[i] * inputArray[i+1] \
+    return max(inputArray[i] * inputArray[i+1] \
     	for i in range(len(inputArray)-1))
 
 adjacentElementsProduct(inputArray)
