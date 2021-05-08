17c3d98965e2a9d8c4f512d9c8829ad76b471896
AmineDachir <68955063+aminedachir@users.noreply.github.com>
2021-05-08 20:28:38 +0100

    Delete main4.py

--- main4.py
+++
@@ -1,6 +0,0 @@
-inputArray = input('enter array :')
-def adjacentElementsProduct(inputArray):
-    return max(inputArray[i] * inputArray[i+1] \
-    	for i in range(len(inputArray)-1))
-
-adjacentElementsProduct(inputArray)
