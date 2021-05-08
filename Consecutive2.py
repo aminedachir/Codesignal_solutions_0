1c1980cf82e9fdfce88a3289968271c6e5286d21
aminedachir <dachiramine110@gmail.com>
2021-05-08 20:25:42 +0100

    update challenge6

--- Consecutive2.py
+++ Consecutive2.py
@@ -1,6 +1,6 @@
 def makeArrayConsecutive2(statues):
-    a = min(statues)
-    c = 0
+    a, c = min(statues), 0
     for i in range(100):
         if (a+1 != statues[i]):
             c = c+1
+    return c
